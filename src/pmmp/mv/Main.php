aababa2fce9b79baea4458978c60ca51b35aff2d
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-02-12 22:38:10 +0200

    Update Main.php

--- src/pmmp/mv/Main.php
+++ src/pmmp/mv/Main.php
@@ -6,15 +6,15 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase { 
 
-    public function onEnable() {  
+    public function onEnable(): void{  
         $this->getLogger()->info("MultiVersion has been enabled!");
     }
   
-    public function onDisable(){
+    public function onDisable(): void{
       $this->getLogger()->info("MultiVerion has been disabled");
     }
   
-    public function onLoad(){
+    public function onLoad(): void{
       $this->getLogger()->info("MultiVersion is Loading..."); 
     }
 }
